d736c8d43ab93d7127b4ff014aa2211504bf833b
imviz <vishnuedappatt@gmail.com>
2022-10-15 14:59:51 +0530

    changes made

--- movie_poster_mangement/settings.py
+++ movie_poster_mangement/settings.py
@@ -216,7 +216,7 @@ AWS_DEFAULT_ACL = 'public-read'
 AWS_LOCATION = 'static'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_DIRS=[
-  'static','build/static',
+  'static',
 ]
 STATIC_URL = 'http://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
